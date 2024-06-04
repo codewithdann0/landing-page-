1faf12350b38879f2a065a772557f27f605d5e15
codewithdann0 <codewithdann028@gmail.com>
2024-06-04 14:28:19 -0700

    80% of the landing still it is not finshed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,86 @@ import './App.css';
 function App() {
   return (
     <>
-    <nav>
+    <nav class="nav-bar">
+    <h1>tier</h1>
       <ul>
-        <h1>MY Landing page</h1>
+        <li><select>
+          <option>WHAT WE CAN DO</option>
+          <option>WHAT WE CAN DO</option>
+          <option>WHAT WE CAN DO</option>
+          <option>WHAT WE CAN DO</option>
+        </select></li>
+        <li><a href='#blog'>BLOG</a></li>
+        <li><a href='#podcast'>PODCAST</a></li>
+        <li><a href='#careers'>CAREERS</a></li>
+        <li><button>WORK WITH US</button></li>
       </ul>
     </nav>
-     
+    <div class="container">
+            <div id="promotion">
+                <h1>Unlock Your Business Potential With Facebook & instagram Adversting</h1>
+                <p>Facebook Premier Level Partner Agency</p>
+               <button>WOKR WITH US</button>
+            </div>
+            <div id="fb-img">
+              <img src='./land-img.png' alt='The graph that show growth'/>
+              </div>
+    </div>
+     <div class="footer">
+         <div id='box'>
+             <div>
+                 <div>
+                   <i class="fa-brands fa-square-facebook fa-2x"></i>
+                   <h5>
+                     Marketing Partner
+                     
+                  </h5>
+                 </div>
+                               
+            </div>    
+             <div>
+              <p>Facebook Premier level Agency Partner</p>
+            </div>
+        </div>
+         <div id='box'>
+              <div>
+                <i class="fa-brands fa-google fa-2x"></i>
+             </div>
+             <div>
+                <p>Google Endorsed Marketing Partner</p>
+             </div>
+         </div>
+         <div id='box'>
+                <div class="forbes">
+                  <h>Forbes</h>
+                </div>
+                <div>
+                <p>Forbes Agency Council Member</p>
+                </div>
+         </div>
+         <div id='box'>
+                  <div>
+                  <h1>Inc</h1>
+      
+                    
+                  </div>
+                  <div>
+                    <p>Inc 5000 Fastest Growing Company</p>
+                  </div>
+         </div>
+         <div id='box'>
+            <div><h1>$100M</h1></div>
+            <div><p>In Annual Digital Ad Spend</p></div>
+         </div>
+         <div id='box'>
+              <div>
+                <h1>15%</h1>
+              </div>
+              <div>
+                  <p>Years Of Facebook Adversting Experience</p>
+              </div>
+         </div>
+     </div>
     </>
   );
 }
